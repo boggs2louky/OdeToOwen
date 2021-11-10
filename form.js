f2a0c4e85bb7e96d7b8e2e74264c349ee4061a6b
Ross Allen <boggs2louky@yahoo.com>
2021-11-09 19:13:49 -0500

    javascript syntac noted out in form.js

--- form.js
+++ form.js
@@ -31,3 +31,39 @@ $(document).ready(function () {
     }
    }) 
 })
+
+/* Javascript syntac 
+console.log('It Works hopefully!')
+
+document.querySelector(document).ready(function () {
+   document.querySelector('.submit').click(function (event) {
+     console.log('Clicked button')
+
+    var email = document.querySelector('.email').value
+    var subject = document.querySelector('.subject').value
+    var message = document.querySelector('.message').value
+    var statusElm = document.querySelector('.status')
+    statusElm.empty()
+    if (email.length > 5 && email.includes ('@') && email.includes('.')) {
+        statusElm.insertAdjacentHTML("beforeend",'<div>Email is valid</div>')
+    } else {
+        event.preventDefault()
+        statusElm.insertAdjacentHTML("beforeend",'<div>Email is not valid</div>')
+    }
+
+    if(subject.length >= 2) {
+        statusElm.insertAdjacentHTML("beforeend",'<div>Subject is valid</div>')
+    } else {
+        event.preventDefault()
+        statusElm.insertAdjacentHTML("beforeend",'<div>Subject is not valid</div>')
+    }
+
+    if(message.length >= 10) {
+        statusElm.insertAdjacentHTML("beforeend",'<div>Message is valid</div>')
+    } else {
+        event.preventDefault()
+        statusElm.insertAdjacentHTML("beforeend",'<div>Message is not valid</div>')    
+    }
+   }) 
+})
+*/
